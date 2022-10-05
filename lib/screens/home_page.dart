223bf2ac4858e1a7baa20e445abb4b013db17fb1
nayemuzzaman <nayemzaman@gmail.com>
2022-10-05 15:34:39 +0600

    tab bar design

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -7,7 +7,9 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late final TabController _tabController = TabController(length: 4, vsync: this);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,6 +24,43 @@ class _HomePageState extends State<HomePage> {
               color: Colors.yellow.shade800,
               thickness: 0.5,
             ),
+            TabBar(
+              controller: _tabController,
+             tabs: [
+              CircleAvatar(
+                backgroundColor: Colors.yellow.shade800,
+                radius: 25,
+              ),
+              CircleAvatar(
+                backgroundColor: Colors.yellow.shade800,
+                radius: 25,
+              ),
+              CircleAvatar(
+                backgroundColor: Colors.yellow.shade800,
+                radius: 25,
+              ),
+              CircleAvatar(
+                backgroundColor: Colors.yellow.shade800,
+                radius: 25,
+              ),
+            ]),
+            Expanded(
+                child: TabBarView(
+                  controller: _tabController,
+             children: const[
+              Center(
+                child: Text("Sofa"),
+              ),
+              Center(
+                child: Text("Bed"),
+              ),
+              Center(
+                child: Text("BathTub"),
+              ),
+              Center(
+                child: Text("Lamp"),
+              ),
+            ]))
           ],
         ),
       ),
@@ -29,26 +68,25 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget homeAppBar() => Row(
-    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    children: [
-      Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: const [
-          Text(
-            "Products",
-          style: TextStyle(
-            fontSize: 28,
-            fontWeight: FontWeight.w800,
-          ),),
-          Text(
-            "Best Household collections",
-          style: TextStyle(
-            fontSize: 12,
-            color: Colors.grey
-          ),)
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Text(
+                "Products",
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+              Text(
+                "Best Household collections",
+                style: TextStyle(fontSize: 12, color: Colors.grey),
+              )
+            ],
+          ),
+          const Icon(Icons.shopping_cart_outlined),
         ],
-      ),
-      const Icon(Icons.shopping_cart_outlined),
-    ],
-  );
+      );
 }
