600ff097f09e13fb6e071134e9fe8b809629d133
nayemuzzaman <nayemzaman@gmail.com>
2022-10-06 19:29:15 +0600

    color choose details page

--- lib/screens/details_page.dart
+++ lib/screens/details_page.dart
@@ -8,6 +8,32 @@ class DetailsPage extends StatefulWidget {
 }
 
 class _DetailsPageState extends State<DetailsPage> {
+  List<Map<String, dynamic>> imagePreviews = [
+    {
+      "color": Colors.yellow.shade800,
+      "image": "yellow_chair.png",
+    },
+    {
+      "color": Colors.pink.shade300,
+      "image": "pink_chair.png",
+    },
+    {
+      "color": Colors.green.shade800,
+      "image": "green_chair.png",
+    },
+    {
+      "color": Colors.blue.shade800,
+      "image": "blue_chair.png",
+    },
+  ];
+  String? selectedImage;
+
+  @override
+  void initState() {
+    selectedImage = imagePreviews[0]["image"];
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
@@ -19,52 +45,53 @@ class _DetailsPageState extends State<DetailsPage> {
         child: Stack(
           children: [
             Container(
-              height: height/2 + 80,
+              height: height / 2 + 80,
               width: width,
               color: Colors.yellow.shade800,
               child: Stack(
                 children: [
                   Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 50),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 50),
                     child: Column(
-                      children: const [
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
                         Text(
                           "Modern Sofa",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                          fontWeight: FontWeight.w700,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w700,
+                          ),
                         ),
-                      ),
-                      Text(
+                        Text(
                           "Household Series",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 13,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      )
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 11,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        )
                       ],
                     ),
                   ),
-
                   Positioned(
                     right: 0,
                     child: Padding(
                       padding: const EdgeInsets.only(top: 30),
                       child: RotatedBox(
                         quarterTurns: 1,
-                        child: Text("household".toUpperCase(),
-                        style: TextStyle(
-                          height: 1,
-                          fontWeight: FontWeight.w900,
-                          fontSize: 60,
-                          color: Colors.white30
-                        ),),
+                        child: Text(
+                          "household".toUpperCase(),
+                          style: TextStyle(
+                              height: 1,
+                              fontWeight: FontWeight.w900,
+                              fontSize: 60,
+                              color: Colors.white30),
+                        ),
                       ),
                     ),
                   ),
-
                   const Center(
                     child: CircleAvatar(
                       backgroundColor: Colors.white,
@@ -75,26 +102,58 @@ class _DetailsPageState extends State<DetailsPage> {
                     right: 110,
                     top: 210,
                     child: Image.asset(
-                      "assets/yellow_chair.png",
+                      "assets/$selectedImage",
                       height: 230,
                       width: 220,
                       fit: BoxFit.cover,
                     ),
                   ),
-                 Positioned(
-                   bottom: 30,
-                   left: 20,
-                   
-                   child: Column(
-                     children: [
-                       Text("More Choice",
-                       style: TextStyle(
-                         fontSize: 13,
-                         color: Colors.white,
-                         fontWeight: FontWeight.w600,
-                       ),)
-                     ],
-                   ))
+                  Positioned(
+                    bottom: 30,
+                    left: 20,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "More Choice",
+                          style: TextStyle(
+                            fontSize: 13,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        Container(
+                          height: 25,
+                          width: 120,
+                          child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: imagePreviews.length,
+                            itemBuilder: (context, index) {
+                              final imagePreview = imagePreviews[index];
+                              return Padding(
+                                padding: const EdgeInsets.only(right: 7),
+                                child: CircleAvatar(
+                                  backgroundColor: Colors.white,
+                                  radius: 12,
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        selectedImage = imagePreview["image"];
+                                      });
+                                    },
+                                    child: CircleAvatar(
+                                      backgroundColor: imagePreview["color"],
+                                      radius: 10,
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
                 ],
               ),
             )
